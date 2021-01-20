efcef6e22ff23c80620a575b7383f89aaaaf8fea
Dmitrii Marianchenko <dima.marjan@gmail.com>
2021-01-20 12:43:28 +0200

    upd

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,11 @@
   const mobileMenuRef = document.querySelector("[data-menu]");
 
   menuBtnRef.addEventListener("click", () => {
-    // const expanded =
-    //   menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
     menuBtnRef.classList.toggle("is-open");
-    // menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
   });
